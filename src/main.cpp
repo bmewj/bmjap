b47a42f50c52d3a4a86755448f4137d6381cdee7
Bartholomew Joyce <bartholomew.michael.joyce@gmail.com>
2021-07-17 15:23:33 +0200

    FFTW stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <ddui/core>
 #include <ddui/app>
 #include <ddui/util/get_asset_filename>
+#include <fftw3/fftw3.h>
+#include <cmath>
 
 #include "stb_vorbis.c"
 #include "audio_client.hpp"
@@ -10,6 +12,7 @@ static Area file_left_channel;
 
 static Area playback_1;
 static Area playback_2;
+
 
 struct Image {
     int image_id;
@@ -32,9 +35,11 @@ static Image img_1;
 static Image img_2;
 static Image img_3;
 static Image img_4;
+static Image img_5;
 static Area slice;
 static Area offset_slice;
 static Area sum_slice;
+static Area another_slice;
 static int offset = 0;
 static void create_peak_image(Image img, Area area, ddui::Color color);
 
@@ -54,6 +59,7 @@ void update() {
         img_2 = create_image(ddui::view.width, 100);
         img_3 = create_image(ddui::view.width, 200);
         img_4 = create_image(ddui::view.width, 200);
+        img_5 = create_image(ddui::view.width, 200);
         create_peak_image(img_1, slice, ddui::rgb(0xff0000));
         create_peak_image(img_2, slice, ddui::rgb(0x0000ff));
         
@@ -106,27 +112,59 @@ void update() {
 
     // Draw offset slice
     {
-        static int previous_offset = -1;
-        if (previous_offset != offset) {
-            previous_offset = offset;
+        static bool did_init = false;
+        static int previous_offset = 0;
+        if (!did_init) {
+            Area ptr = sum_slice;
+            while (ptr < ptr.end) {
+                *ptr++ = 0.0;
+            }
+        }
+        if (!did_init || previous_offset != offset) {
 
-            int i = 0;
-            Area ptr_in_1 = slice;
-            Area ptr_in_2 = slice;
-            Area ptr_out = offset_slice;
-            if (offset >= 0) {
-                ptr_in_1 += offset;
-                ptr_out += offset;
-            } else {
-                ptr_in_2 += -offset;
+            // Compute auto correlation at offset "offset"
+            {
+                Area ptr_in_1 = slice;
+                Area ptr_in_2 = slice;
+                Area ptr_out = offset_slice;
+                if (offset >= 0) {
+                    ptr_in_1 += offset;
+                    ptr_out += offset;
+                } else {
+                    ptr_in_2 += -offset;
+                }
+
+                while (ptr_in_1 < ptr_in_1.end && ptr_out < ptr_out.end && ptr_in_2 < ptr_in_2.end) {
+                    *ptr_out++ = *ptr_in_1++ * *ptr_in_2++;
+                }
             }
 
-            while (ptr_in_1 < ptr_in_1.end && ptr_out < ptr_out.end && ptr_in_2 < ptr_in_2.end) {
-                *ptr_out++ = 0.5 * *ptr_in_1++ + 0.5 * *ptr_in_2++;
+            // Sum it all up
+            {
+                Area ptr_offset = offset_slice + (offset > 0 ? offset : 0);
+                double sum = 0.0;
+                static double sum_all_squared = 0.0;
+                for (; ptr_offset < ptr_offset.end; ++ptr_offset) {
+                    sum += *ptr_offset;
+                }
+
+                if (offset == 0) {
+                    sum_all_squared = sum;
+                }
+                
+                auto offset_min = offset < previous_offset ? offset : previous_offset;
+                auto offset_max = offset > previous_offset ? offset : previous_offset;
+                Area ptr_out = sum_slice + offset_min;
+                Area ptr_out_end = sum_slice + offset_max;
+                while (ptr_out < ptr_out.end && ptr_out < ptr_out_end.ptr) {
+                    *ptr_out++ = sum / sum_all_squared;
+                }
             }
             
-            create_peak_image(img_3, offset_slice, ddui::rgb(0xff00ff));
+            previous_offset = offset;
+            create_peak_image(img_3, sum_slice, ddui::rgb(0x009900));
         }
+        did_init = true;
 
         ddui::save();
         ddui::translate(0, 200);
@@ -138,45 +176,21 @@ void update() {
         ddui::fill();
         ddui::restore();
     }
-
-    // Draw sum slice
+    
+    // Draw another slice
     {
         static bool did_init = false;
-        static int previous_offset = 0;
         if (!did_init) {
-            Area ptr = sum_slice;
-            while (ptr < ptr.end) {
-                *ptr++ = 0.0;
-            }
+            did_init = true;
+            create_peak_image(img_5, another_slice, ddui::rgb(0xff0000));
         }
-        if (!did_init || previous_offset != offset) {
-            Area ptr_offset = offset_slice + (offset > 0 ? offset : 0);
-            float max = 0.0;
-            for (; ptr_offset < ptr_offset.end; ++ptr_offset) {
-                if (max < *ptr_offset) {
-                    max = *ptr_offset;
-                }
-            }
-
-            auto offset_min = offset < previous_offset ? offset : previous_offset;
-            auto offset_max = offset > previous_offset ? offset : previous_offset;
-            Area ptr_out = sum_slice + offset_min;
-            Area ptr_out_end = sum_slice + offset_max;
-            while (ptr_out < ptr_out.end && ptr_out < ptr_out_end.ptr) {
-                *ptr_out++ = max;
-            }
-
-            previous_offset = offset;
-            create_peak_image(img_4, sum_slice, ddui::rgb(0x009900));
-        }
-        did_init = true;
 
         ddui::save();
         ddui::translate(0, 400);
 
-        auto paint = ddui::image_pattern(0, 0, img_4.width, img_4.height, 0.0f, img_4.image_id, 1.0f);
+        auto paint = ddui::image_pattern(0, 0, img_5.width, img_5.height, 0.0f, img_5.image_id, 1.0f);
         ddui::begin_path();
-        ddui::rect(0, 0, img_4.width, img_4.height);
+        ddui::rect(0, 0, img_5.width, img_5.height);
         ddui::fill_paint(paint);
         ddui::fill();
         ddui::restore();
@@ -190,9 +204,9 @@ void read_callback(int num_samples, int num_areas, Area* areas) {
 void write_callback(int num_samples, int num_areas, Area* areas) {
 
 //    constexpr int SAMPLE_RATE = 44100;
-//    constexpr double FREQUENCY = 20000;
+//    double frequency = (double)SAMPLE_RATE / max_i;
 //
-//    double time_period = SAMPLE_RATE / FREQUENCY;
+//    double time_period = SAMPLE_RATE / frequency;
 //
 //    auto out_l = areas[0];
 //    auto out_r = areas[1];
@@ -233,6 +247,7 @@ void convert_samples(int num_samples, short* input, float* output) {
     }
 }
 
+static void do_fft();
 
 int main(int argc, const char** argv) {
 
@@ -269,6 +284,8 @@ int main(int argc, const char** argv) {
     playback_1 = file_left_channel;
     playback_2 = file_left_channel;
     
+    do_fft();
+
     init_audio_client(sample_rate, read_callback, write_callback);
 
     ddui::app_run();
@@ -328,6 +345,19 @@ void create_peak_image(Image img, Area area, ddui::Color color) {
             y1 = height * (1.0 - (min_value + 1.0) * 0.5);
         }
 
+        if (y0 < 0) {
+            y0 = 0;
+        }
+        if (y0 >= height) {
+            y0 = height;
+        }
+        if (y1 < 0) {
+            y1 = 0;
+        }
+        if (y1 >= height) {
+            y1 = height;
+        }
+
         // Fill in the pixels
         for (auto y = y0; y <= y1; ++y) {
             auto i = 4 * (width * y + x);
@@ -346,4 +376,131 @@ static void color_to_bytes(ddui::Color in, unsigned char* out) {
     out[1] = (unsigned char)(in.g * 255.0f);
     out[2] = (unsigned char)(in.b * 255.0f);
     out[3] = (unsigned char)(in.a * 255.0f);
+}
+
+void do_fft() {
+    auto N = (int)exp2(ceil(log2(WINDOW_LENGTH)));
+
+    fftw_plan plan, plan_2;
+    auto in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
+    auto out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
+    plan = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+    plan_2 = fftw_plan_dft_1d(N, out, in, FFTW_BACKWARD, FFTW_ESTIMATE);
+
+    // .. fill in with data
+    {
+        auto window = file_left_channel + 1000;
+        window.end = window.ptr + WINDOW_LENGTH * window.step;
+        auto ptr_in = in;
+        auto ptr_in_end = in + N;
+        // Copy the window into our double array
+        while (window < window.end) {
+            (*ptr_in)[0] = *window++;
+            (*ptr_in)[1] = 0.0;
+            ptr_in++;
+        }
+        // Zero pad the input
+        while (ptr_in < ptr_in_end) {
+            (*ptr_in)[0] = 0.0;
+            (*ptr_in)[1] = 0.0;
+            ptr_in++;
+        }
+    }
+
+    printf("transforming...\n");
+    fftw_execute(plan);
+    {
+        auto ptr = out;
+        auto ptr_end = out + N;
+        auto scalar = 1.0 / std::sqrt((double)N);
+        for (; ptr < ptr_end; ++ptr) {
+            auto real = (*ptr)[0] * scalar;
+            auto imag = (*ptr)[1] * scalar;
+            (*ptr)[0] = real * real + imag * imag;
+            (*ptr)[1] = 0.0;
+        }
+        ptr = out;
+    }
+
+    printf("inverse transforming...\n");
+    fftw_execute(plan_2);
+    {
+        auto ptr = (double*)in;
+        auto ptr_end = (double*)in + N*2;
+        auto scalar = 1.0 / *ptr; // the first sample is the max
+        for (; ptr < ptr_end; ++ptr) {
+            *ptr *= scalar;
+        }
+    }
+    printf("done.\n");
+
+    // Saving output to area
+    another_slice = Area(new float[WINDOW_LENGTH], WINDOW_LENGTH, 1);
+    {
+        auto slice_out = another_slice;
+//        another_slice.end = another_slice.ptr + 200;
+        auto ptr_in = in;
+        while (slice_out < slice_out.end) {
+            *slice_out++ = (*ptr_in++)[0];
+        }
+    }
+
+    // Computing frequency using auto correlation
+    {
+        auto ptr = in;
+        auto ptr_end = in + WINDOW_LENGTH;
+        
+        // Skip first peak until we cross below zero
+        for (; ptr < ptr_end && (*ptr)[0] > 0.0; ++ptr) {}
+
+        // Find max value
+        int max_i = 0;
+        for (auto max = (*ptr)[0]; ptr < ptr_end; ++ptr) {
+            if (max < (*ptr)[0]) {
+                max = (*ptr)[0];
+                max_i = ptr - in;
+            }
+        }
+
+        constexpr double SAMPLE_RATE = 44100.0;
+        auto frequency = SAMPLE_RATE / max_i;
+
+        constexpr const char* NOTE_NAMES[12] = {
+            "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"
+        };
+
+        // A4 = 440Hz -> A0 = 27.5Hz
+        constexpr double FREQ_A0 = 27.5;
+        auto note_value = 12.0 * log(frequency / FREQ_A0) / log(2.0);
+        auto note_value_fixed = (int)round(note_value);
+        auto note_value_cents = (note_value - note_value_fixed) * 100;
+        auto note_value_octave = note_value_fixed / 12;
+        auto note = NOTE_NAMES[note_value_fixed % 12];
+        printf("Detected: %s%d + %f (%fHz)\n", note, note_value_octave, note_value_cents, frequency);
+    }
+
+    // Computing frequency using FFT frequency bins
+//    {
+//        auto ptr = out;
+//        auto ptr_end = out + N;
+//
+//        // Find max value
+//        int max_i = 0;
+//        for (auto max = (*ptr)[0]; ptr < ptr_end; ++ptr) {
+//            if (max < (*ptr)[0]) {
+//                max = (*ptr)[0];
+//                max_i = ptr - out;
+//            }
+//        }
+//
+//        constexpr double SAMPLE_RATE = 44100.0;
+//        auto frequency = max_i * (SAMPLE_RATE / N);
+//        printf("%fHz\n", frequency);
+//    }
+
+    // ... repeat as needed
+    fftw_destroy_plan(plan);
+    fftw_destroy_plan(plan_2);
+    fftw_free(in);
+    fftw_free(out);
 }
